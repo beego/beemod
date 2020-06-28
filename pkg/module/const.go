13c72bb4701ca8d9c5b58989eb0ac529360a3bf4
yumancang <mancangluo@tencent.com>
2020-06-28 20:51:22 +0800

    oauth2 client

--- pkg/module/const.go
+++ pkg/module/const.go
@@ -4,9 +4,12 @@ const ConfigPrefix = "beego."
 
 const OssName = "oss"
 
+const Oauth2Name = "oauth2"
+
 // order invokers
 var OrderInvokers = []invokerAttr{
 	{OssName},
+	{Oauth2Name},
 }
 
 type invokerAttr struct {
