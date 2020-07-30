b9be3647aeff6168727636eb8c8da5f8cc1b41e8
myxy99 <2399158611@qq.com>
2020-07-30 11:47:07 +0800

    social

--- pkg/social/social_test.go
+++ pkg/social/social_test.go
@@ -11,19 +11,19 @@ import (
 )
 
 const configTpl = `
-[beemod.oauth2.qq]
+[beego.oauth2.qq]
 	debug = true
 	mode  = "qq"
 	app_id  = "app_id"
 	app_secret  = "app_secret"
 	redirectURI = "www.beego.com"
-[beemod.oauth2.wx]
+[beego.oauth2.wx]
 	debug = true
 	mode  = "wx"
 	app_id  = "app_id"
 	app_secret  = "app_secret"
 	redirectURI = "www.beego.com"
-[beemod.oauth2.github]
+[beego.oauth2.github]
 	debug = true
 	mode  = "github"
 	app_id  = "app_id"
