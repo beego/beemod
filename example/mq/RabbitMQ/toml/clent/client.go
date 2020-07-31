39578ae9a16214d094a0b2a0bd66103bd73007c9
myxy99 <2399158611@qq.com>
2020-07-31 21:15:54 +0800

    rabbitmq

--- example/mq/RabbitMQ/toml/clent/client.go
+++ example/mq/RabbitMQ/toml/clent/client.go
@@ -1,27 +1,27 @@
 /**
 * @Author: myxy99 <myxy99@foxmail.com>
-* @Date: 2020/7/31 19:22
+* @Date: 2020/7/31 19:21
  */
 package main
 
 import (
 	"github.com/beego/beemod"
 	"github.com/beego/beemod/pkg/mq/rabbitmq"
-	"time"
+	"github.com/streadway/amqp"
+	"log"
 )
 
 func main() {
 	err := beemod.Register(
 		rabbitmq.DefaultBuild,
-	).SetCfg("./config.toml", "toml").Run()
+	).SetCfg("../config.toml", "toml").Run()
 	if err != nil {
 		panic("register err:" + err.Error())
 	}
 	obj := rabbitmq.Invoker("dev")
 
 	mq := obj.NewRabbitMQ("simple", "test")
-	for {
-		_ = mq.Publish("test")
-		time.Sleep(3 * time.Second)
-	}
+	mq.Receive(func(delivery amqp.Delivery) {
+		log.Print(string(delivery.Body) + "\n")
+	})
 }
--- example/mq/RabbitMQ/toml/service/service.go
+++ example/mq/RabbitMQ/toml/service/service.go
@@ -1,27 +1,27 @@
 /**
 * @Author: myxy99 <myxy99@foxmail.com>
-* @Date: 2020/7/31 19:21
+* @Date: 2020/7/31 19:22
  */
 package main
 
 import (
 	"github.com/beego/beemod"
 	"github.com/beego/beemod/pkg/mq/rabbitmq"
-	"github.com/streadway/amqp"
-	"log"
+	"time"
 )
 
 func main() {
 	err := beemod.Register(
 		rabbitmq.DefaultBuild,
-	).SetCfg("./config.toml", "toml").Run()
+	).SetCfg("../config.toml", "toml").Run()
 	if err != nil {
 		panic("register err:" + err.Error())
 	}
 	obj := rabbitmq.Invoker("dev")
 
 	mq := obj.NewRabbitMQ("simple", "test")
-	mq.Receive(func(delivery amqp.Delivery) {
-		log.Print(string(delivery.Body) + "\n")
-	})
+	for {
+		_ = mq.Publish("test")
+		time.Sleep(3 * time.Second)
+	}
 }
